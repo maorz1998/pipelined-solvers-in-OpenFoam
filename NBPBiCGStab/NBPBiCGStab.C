57d8b9c9824f47bbca89516a52efbde105a2de2d
maorz1998 <maorz1998@stu.pku.edu.cn>
2023-02-19 22:29:06 +0800

    construct pipelined PBiCGStab solver (ongoing)

--- NBPBiCGStab/NBPBiCGStab.C
+++ NBPBiCGStab/NBPBiCGStab.C
@@ -89,6 +89,20 @@ Foam::solverPerformance Foam::NBPBiCGStab::solve
     scalarField yA(nCells);
     scalar* __restrict__ yAPtr = yA.begin();
 
+    scalarField rM(nCells);
+    scalar* __restrict__ rMPtr = rM.begin();
+
+    scalarField wA(nCells);
+    scalar* __restrict__ wAPtr = wA.begin();
+
+    scalarField wM(nCells);
+    scalar* __restrict__ wMPtr = wM.begin();
+
+    scalarField tA(nCells);
+    scalar* __restrict__ tAPtr = tA.begin();
+
+    scalar alpha, beta;
+
     // --- Calculate A.psi
     matrix_.Amul(yA, psi, interfaceBouCoeffs_, interfaces_, cmpt);
 
@@ -96,8 +110,28 @@ Foam::solverPerformance Foam::NBPBiCGStab::solve
     scalarField rA(source - yA);
     scalar* __restrict__ rAPtr = rA.begin();
 
+    scalarField r0(rA);
+
     // --- Calculate normalisation factor
     const scalar normFactor = this->normFactor(psi, source, yA, pA);
+
+    // --- Select and construct the preconditioner
+    autoPtr<lduMatrix::preconditioner> preconPtr =
+    lduMatrix::preconditioner::New
+    (
+        *this,
+        controlDict_
+    );
+    preconPtr->precondition(rM, rA, cmpt);
+    matrix_.Amul(wA, rM, interfaceBouCoeffs_, interfaces_, cmpt);
+    preconPtr->precondition(wM, wA, cmpt);
+
+    // initialize alpha & beta
+    const scalar rArA = gSumSqr(rA, matrix().mesh().comm());
+    scalar algha = rArA/gSumProd(rA, wA, matrix().mesh().comm());
+    scalar beta = 0;
+    scalar omega = 0;
+    scalar r0rA = rArA;
 
     if (lduMatrix::debug >= 2)
     {
@@ -117,8 +151,12 @@ Foam::solverPerformance Foam::NBPBiCGStab::solve
      || !solverPerf.checkConvergence(tolerance_, relTol_)
     )
     {
-        scalarField AyA(nCells);
-        scalar* __restrict__ AyAPtr = AyA.begin();
+        // Initialize parameters
+        scalarField pM(nCells);
+        scalar* __restrict__ pMPtr = pM.begin();
+
+        scalarField sM(nCells);
+        scalar* __restrict__ sMPtr = sM.begin();
 
         scalarField sA(nCells);
         scalar* __restrict__ sAPtr = sA.begin();
@@ -126,89 +164,43 @@ Foam::solverPerformance Foam::NBPBiCGStab::solve
         scalarField zA(nCells);
         scalar* __restrict__ zAPtr = zA.begin();
 
-        scalarField tA(nCells);
-        scalar* __restrict__ tAPtr = tA.begin();
+        scalarField zM(nCells);
+        scalar* __restrict__ zMPtr = zM.begin();
+
+        scalarField vA(nCells);
+        scalar* __restrict__ vAPtr = vA.begin();
+
+        scalarField qA(nCells);
+        scalar* __restrict__ qAPtr = qA.begin();
+
+        scalarField qM(nCells);
+        scalar* __restrict__ qMPtr = qM.begin();
 
         // --- Store initial residual
         const scalarField rA0(rA);
 
-        // --- Initial values not used
-        scalar rA0rA = 0;
-        scalar alpha = 0;
-        scalar omega = 0;
-
-        // --- Select and construct the preconditioner
-        autoPtr<lduMatrix::preconditioner> preconPtr =
-        lduMatrix::preconditioner::New
-        (
-            *this,
-            controlDict_
-        );
-
         // --- Solver iteration
         do
         {
-            // --- Store previous rA0rA
-            const scalar rA0rAold = rA0rA;
-
-            rA0rA = gSumProd(rA0, rA, matrix().mesh().comm());
-
-            // --- Test for singularity
-            if (solverPerf.checkSingularity(mag(rA0rA)))
-            {
-                break;
-            }
-
-            // --- Update pA
-            if (solverPerf.nIterations() == 0)
-            {
-                for (label cell=0; cell<nCells; cell++)
-                {
-                    pAPtr[cell] = rAPtr[cell];
-                }
-            }
-            else
-            {
-                // --- Test for singularity
-                if (solverPerf.checkSingularity(mag(omega)))
-                {
-                    break;
-                }
-
-                const scalar beta = (rA0rA/rA0rAold)*(alpha/omega);
-
-                for (label cell=0; cell<nCells; cell++)
-                {
-                    pAPtr[cell] =
-                        rAPtr[cell] + beta*(pAPtr[cell] - omega*AyAPtr[cell]);
-                }
-            }
-
-            // --- Precondition pA
-            preconPtr->precondition(yA, pA, cmpt);
-
-            // --- Calculate AyA
-            matrix_.Amul(AyA, yA, interfaceBouCoeffs_, interfaces_, cmpt);
-
-            const scalar rA0AyA = gSumProd(rA0, AyA, matrix().mesh().comm());
-
-            alpha = rA0rA/rA0AyA;
-
-            // --- Calculate sA
             for (label cell=0; cell<nCells; cell++)
             {
-                sAPtr[cell] = rAPtr[cell] - alpha*AyAPtr[cell];
+                pMPtr[cell] = rMPtr[cell] + beta*(pMPtr[cell] - omega*sMPtr[cell]);
+                sAPtr[cell] = wAPtr[cell] + beta*(sAPtr[cell] - omega*zAPtr[cell]);
+                sMPtr[cell] = wMPtr[cell] + beta*(sMPtr[cell] - omega*zMPtr[cell]);
+                zAPtr[cell] = tAPtr[cell] + beta*(zAPtr[cell] - omega*vAPtr[cell]);
+                qAPtr[cell] = rAPtr[cell] - alpha*sAPtr[cell];
+                qMPtr[cell] = rMPtr[cell] - alpha*sMPtr[cell];
+                yAPtr[cell] = wAPtr[cell] - alpha*zAPtr[cell];
             }
-
-            // --- Test sA for convergence
+            // check convergence
             solverPerf.finalResidual() =
-                gSumMag(sA, matrix().mesh().comm())/normFactor;
+                gSumMag(qA, matrix().mesh().comm())/normFactor;
 
             if (solverPerf.checkConvergence(tolerance_, relTol_))
             {
                 for (label cell=0; cell<nCells; cell++)
                 {
-                    psiPtr[cell] += alpha*yAPtr[cell];
+                    psiPtr[cell] += alpha*pMPtr[cell];
                 }
 
                 solverPerf.nIterations()++;
@@ -216,35 +208,87 @@ Foam::solverPerformance Foam::NBPBiCGStab::solve
                 return solverPerf;
             }
 
-            // --- Precondition sA
-            preconPtr->precondition(zA, sA, cmpt);
-
-            // --- Calculate tA
-            matrix_.Amul(tA, zA, interfaceBouCoeffs_, interfaces_, cmpt);
-
-            const scalar tAtA = gSumSqr(tA, matrix().mesh().comm());
-
-            // --- Calculate omega from tA and sA
-            //     (cheaper than using zA with preconditioned tA)
-            omega = gSumProd(tA, sA, matrix().mesh().comm())/tAtA;
-
-            // --- Update solution and residual
+            // --- overlap comm and comp
+            // 1. no blocking reduction 
+            int allredSend1[2];
+            int allredRecv1[2];
+            allredSend1[0] = sumProd(qA, yA);
+            allredSend1[1] = sumSqr(yA);
+            MPI_Request reqs;
+            MPI_Iallreduce
+            (
+                &allredSend1,
+                &allredRecv1,
+                2,
+                MPI_SCALAR,
+                MPI_SUM,
+                Pstream::msgType(),
+                matrix().mesh().comm(),
+                &reqs
+            );
+            // 2. SPMV
+            preconPtr->precondition(zM, zA, cmpt);
+            matrix_.Amul(vA, zM, interfaceBouCoeffs_, interfaces_, cmpt);
+            // 3. MPI_wait
+            MPI_Wait(&reqs, MPI_STATUS_IGNORE);
+            
+            omega = allredSend1[0] / allredSend1[1];
             for (label cell=0; cell<nCells; cell++)
             {
-                psiPtr[cell] += alpha*yAPtr[cell] + omega*zAPtr[cell];
-                rAPtr[cell] = sAPtr[cell] - omega*tAPtr[cell];
+                psiPtr[cell] += alpha*pMPtr[cell] + omega*qMPtr[cell];
+                rAPtr[cell] = qAPtr[cell] - omega*yAPtr[cell];
+                rMPtr[cell] = qMPtr[cell] - omega*(qMPtr[cell] - alpha*zMPtr[cell]);
+                wAPtr[cell] = yAPtr[cell] - omega*(tAPtr[cell] - alpha*vAPtr[cell]);
+            }
+            // check convergence
+            solverPerf.finalResidual() =
+                gSumMag(rA, matrix().mesh().comm())/normFactor;
+
+            if (solverPerf.checkConvergence(tolerance_, relTol_))
+            {
+                for (label cell=0; cell<nCells; cell++)
+                {
+                    psiPtr[cell] += alpha*pMPtr[cell];
+                }
+
+                solverPerf.nIterations()++;
+
+                return solverPerf;
             }
 
-            solverPerf.finalResidual() =
-                gSumMag(rA, matrix().mesh().comm())
-               /normFactor;
+            // --- overlap comm and comp
+            // 1. no blocking reduction 
+            int allredSend2[4];
+            int allredRecv2[4];
+            allredSend2[0] = sumProd(r0, rA);
+            allredSend2[1] = sumProd(r0, wA);
+            allredSend2[2] = sumProd(r0, sA);
+            allredSend2[3] = sumProd(r0, zA);
+            MPI_Request reqs;
+            MPI_Iallreduce
+            (
+                &allredSend2,
+                &allredRecv2,
+                4,
+                MPI_SCALAR,
+                MPI_SUM,
+                Pstream::msgType(),
+                matrix().mesh().comm(),
+                &reqs
+            );
+            // 2. SPMV
+            preconPtr->precondition(wM, wA, cmpt);
+            matrix_.Amul(tA, wM, interfaceBouCoeffs_, interfaces_, cmpt);
+            // 3. MPI_wait
+            MPI_Wait(&reqs, MPI_STATUS_IGNORE);
+
+            beta = (alpha/omega)*allredRecv2[0]/r0rA;
+            alpha = allredRecv2[0]/(allredRecv2[1] + beta*allredRecv2[2] - beta*omega*allredRecv2[3]);
+            r0rA = allredRecv2[0];
+
         } while
         (
-            (
-              ++solverPerf.nIterations() < maxIter_
-            && !solverPerf.checkConvergence(tolerance_, relTol_)
-            )
-         || solverPerf.nIterations() < minIter_
+            ++solverPerf.nIterations() < maxIter_ || solverPerf.nIterations() < minIter_
         );
     }
 
